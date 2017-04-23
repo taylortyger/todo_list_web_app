7bb5178e1b88f4287d70d4dee54031cf5f241f41
Taylor Tyger <taylortyger@gmail.com>
2017-04-23 17:48:59 +0000

    Restarted project and app

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for todo_list_web_app project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo_list_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
