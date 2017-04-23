e9262b0e900ce807e2ac9ab2bf5a830e981e1515
Taylor Tyger <taylortyger@gmail.com>
2017-04-23 18:53:50 +0000

    Started database schema

--- todo_list/models.py
+++ todo_list/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=150)
+    pub_date = models.DateTimeField('date created')
+    def __str__(self):
+        return self.title
+        
+    
+class Task(models.Model):
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    task_text = models.CharField(max_length=300)
+    completed = models.BooleanField(default=False)
+    def __str__(self):
+        return self.task_text
+        
+    
